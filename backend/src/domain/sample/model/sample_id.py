1d0565942cb9febf0053b16f1db863bca1c72bdd
kento yokozuka <yokozuka.kento@gmail.com>
2025-04-25 00:18:01 -0700

    Change dir name

--- backend/src/domain/sample/model/sample_id.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from pydantic import BaseModel
-
-
-@dataclass(frozen=True)
-class SampleID(BaseModel):
-    value: int
--- backend/src/domain/shared/created_at.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-
-@dataclass(frozen=True)
-class CreatedAt:
-    value: datetime
